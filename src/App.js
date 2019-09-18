46b4ce3016ffd41641a48ba58382864c8d0a686f
Megha Niharika H S <megha.hs@BRILLIO.COM>
2019-09-18 17:27:28 +0530

    onclick using bind func and state with class component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 
 import React from "react"
 
+class App extends React.Component{
 
-class  App extends React.Component{
-
- constructor()
- {
-   super()
-   this.state = {
-     unreadmsg :[ 
-       "abhay girl",
-        "vivi "
-      ]
-   }
- }
- render()
- {
-  return (
-    <div>
-<h2>you have {this.state.unreadmsg.length} unread msgs</h2>
-      </div>
-  )
- }
-
+  constructor()
+  {
+    super()
+this.state =
+{
+  isLoggedIn :false
 }
+this.handclick = this.handclick.bind(this)
+  }
+
+  handclick()
+  {
+    console.log("clicking")
+  }
+  render()
+  {
+    return(
+      <div>
+<button onClick ={this.handclick}>Log in</button>
 
 
+        </div>
+    )
+  }
+}
 
 
 export default App
